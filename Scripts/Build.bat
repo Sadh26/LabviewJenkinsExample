b6c0eebf9a20bf467f2f065c1d8dfba185d194cc
Sadh26 <135430869+Sadh26@users.noreply.github.com>
2023-06-17 19:55:19 +0530

    Update Build.bat

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,3 +1,3 @@
-SET cur_dir=%cd%
+SET cur_dir=%cd Windows\System32%
 echo %cur_dir%
 LabVIEWCLI -LogToConsole true -OperationName ExecuteBuildSpec -ProjectPath "%cur_dir%\LabviewJenkinsExample.lvproj" -BuildSpecName "App"
